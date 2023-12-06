0040d9536187e9e64225e03eb7a861446ec7c748
cwshugg <connor@shugg.dev>
2023-12-06 08:11:42 -0500

    added tap dance to change between shift and caps lock

--- crkbd/td.h
+++ crkbd/td.h
@@ -11,6 +11,7 @@
 typedef enum __P(td_code)
 {
     TD_TG,          // Tab with one tap, grave with double-tap
+    TD_SC,          // Shift with one tap, caps lock with double-tap
 } __P(td_code_e);
 
 #endif
